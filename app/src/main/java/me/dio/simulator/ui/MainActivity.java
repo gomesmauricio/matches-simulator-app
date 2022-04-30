b0effd165e49006ce629fad34fb4a3a6eec14d67
Mauricio <josemauricio.gomes@gmail.com>
2022-04-29 21:16:48 -0300

    Adicionando crash lytics

--- app/src/main/java/me/dio/simulator/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator/ui/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+//            throw new RuntimeException("Teste crashlytics");
         });
 
     }
